2adfab02aadd3df51c54b7ace826e502293c4e87
Bwijn <779805126@qq.com>
2019-10-10 21:51:41 +0800

    初次提交

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+
+            alias: {
+                'assets': '@/assets',
+                'views': '@/views',
+                'components': '@/components',
+                'network': '@/network',
+                'commonjs': '@/commonjs',
+
+            } // 别名配置
+        }
+    }
+}
